441f069eac33a4fb52c842e64107a4072f83ff3f
Sloth <austere4@gmail.com>
2024-05-25 23:12:13 +0300

    Program added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int x = 301;
+        int y = 20;
+        int bonus = x / y;
+        System.out.println("Итоговый бонус:" + bonus);
+    }
 }
